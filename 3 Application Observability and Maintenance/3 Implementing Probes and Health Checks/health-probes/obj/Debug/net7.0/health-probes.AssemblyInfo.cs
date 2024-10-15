b9091549adea9cdbfd980088f40090749b36a2d6
Geluku <gelu.liuta@gmail.com>
2024-10-15 08:28:26 +0200

    added solution

--- 3 Application Observability and Maintenance/3 Implementing Probes and Health Checks/health-probes/obj/Debug/net7.0/health-probes.AssemblyInfo.cs
+++ 3 Application Observability and Maintenance/3 Implementing Probes and Health Checks/health-probes/obj/Debug/net7.0/health-probes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("health-probes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+302d82574470e04bbe3a3f18ff2aa5fe18658ced")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e412b80a1b9427741e9d94b3f7eed5644ebde22")]
 [assembly: System.Reflection.AssemblyProductAttribute("health-probes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("health-probes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
